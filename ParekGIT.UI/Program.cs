1eeb215769a565812e6421857c116b3677c4864d
Parek4Life <125895505+Cherttov@users.noreply.github.com>
2026-04-27 18:33:56 +0200

    Initial commit

---
+++ ParekGIT.UI/Program.cs
@@ -0,0 +1,23 @@
+﻿using Photino.NET;
+using System.Drawing;
+using System.Text;
+
+namespace ParekGIT.UI
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var window = new PhotinoWindow()
+                .SetTitle("ParekGIT")
+                .SetUseOsDefaultSize(false)
+                .SetSize(new Size(960, 660))
+                .SetMinSize(960, 660)
+                .Center()
+                .Load("wwwroot/index.html");
+
+            window.WaitForClose();
+        }
+    }
+}
