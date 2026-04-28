da593743810119d492c23e2a823b92856b09cc2b
Parek4Life <125895505+Cherttov@users.noreply.github.com>
2026-04-28 16:58:07 +0200

    Implemented splitter for main-content/right-sidebar

---
+++ ParekGIT.UI/wwwroot/script.js
@@ -0,0 +1,39 @@
+const leftSidebar = document.getElementById("left-sidebar");
+const mainContent = document.getElementById("main-content");
+const rightSidebar = document.getElementById("right-sidebar");
+const resizer = document.getElementById("resizer");
+
+let minRightWidth, maxRightWidth;
+let isResizing = false;
+
+// Split Container
+resizer.addEventListener("mousedown", (event) => {
+    isResizing = true;
+
+    let rightCSS = window.getComputedStyle(rightSidebar);
+    let mainCSS = window.getComputedStyle(mainContent);
+
+    minRightWidth = parseInt(rightCSS.minWidth) || 192;
+    let minMainWidth = parseInt(mainCSS.minWidth) || 360;
+
+    maxRightWidth = window.innerWidth - leftSidebar.offsetWidth - minMainWidth - resizer.offsetWidth;
+
+    document.addEventListener("mousemove", resize);
+    document.addEventListener("mouseup", stopResize);
+    document.body.style.userSelect = "none";
+});
+
+function resize(event) {
+    if (isResizing) {
+        let targetWidth = window.innerWidth - event.clientX;
+        let newWidth = Math.max(minRightWidth, Math.min(targetWidth, maxRightWidth));
+        rightSidebar.style.width = `${newWidth}px`;
+    }
+}
+
+function stopResize() {
+    isResizing = false;
+    document.removeEventListener("mousemove", resize);
+    document.removeEventListener("mouseup", stopResize);
+    document.body.style.userSelect = "";
+}
