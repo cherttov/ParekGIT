87664b095ba56b56758329e18c2659c7d26bae20
Parek4Life <125895505+Cherttov@users.noreply.github.com>
2026-09-08 21:11:10 +0200

    Implemented RepoPushHandler

---
+++ ParekGIT.Bridge/Handlers/RepoPushHandler.cs
@@ -0,0 +1,40 @@
+﻿using ParekGIT.Bridge.Data;
+using ParekGIT.Bridge.Interfaces;
+using ParekGIT.Bridge.Models;
+using ParekGIT.Core.Interfaces;
+using Photino.NET;
+using System.Text.Json;
+
+namespace ParekGIT.Bridge.Handlers
+{
+	public class RepoPushHandler : IMessageHandler
+	{
+		private readonly PhotinoWindow _window;
+		private readonly IGitRunner _gitRunner;
+
+		public string Action => "REPO_PUSH";
+
+		// Constructor
+		public RepoPushHandler(PhotinoWindow window, IGitRunner gitRunner)
+		{
+			_window = window;
+			_gitRunner = gitRunner;
+		}
+
+		public async Task ExecuteAsync(JsonElement payload)
+		{
+			string repoPath = payload.GetProperty("repoPath").GetString()
+				?? throw new IpcPayloadException("repoPath");
+
+			await _gitRunner.PushAsync(repoPath);
+
+			// Response
+			var response = new IpcMessage
+			{
+				Action = "REPO_PUSHED",
+				Payload = JsonSerializer.SerializeToElement(new { success = true })
+			};
+			_window.SendWebMessage(JsonSerializer.Serialize(response));
+		}
+	}
+}
