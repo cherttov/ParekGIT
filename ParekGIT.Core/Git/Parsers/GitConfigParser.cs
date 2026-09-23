105a1902bfaf95459dc43afbc42ffe3e22960b4f
Parek4Life <125895505+Cherttov@users.noreply.github.com>
2026-09-23 18:58:23 +0200

    Added GitConfigParser.cs

---
+++ ParekGIT.Core/Git/Parsers/GitConfigParser.cs
@@ -0,0 +1,25 @@
+﻿using ParekGIT.Core.Models;
+
+namespace ParekGIT.Core.Git.Parsers
+{
+	internal static class GitConfigParser
+	{
+		internal static GitConfigInfo Parse(string rawOutput)
+		{
+			var config = new GitConfigInfo { Name = string.Empty, Email = string.Empty };
+			if (string.IsNullOrWhiteSpace(rawOutput)) { return config; }
+
+			var lines = rawOutput.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+			foreach (var line in lines)
+			{
+				var parts = line.Split(' ', 2);
+				if (parts.Length < 2) { continue; }
+
+				if (parts[0] == "user.name") { config.Name = parts[1].Trim(); }
+				else if (parts[0] == "user.email") { config.Email = parts[1].Trim(); }
+			}
+
+			return config;
+		}
+	}
+}
